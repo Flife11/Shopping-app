dc7258efb9192ffe8d85183845696aa773271d26
21120551 <damtutam@gmail.com>
2024-01-12 16:25:29 +0700

    add todo

--- Main/middleware/passport.js
+++ Main/middleware/passport.js
@@ -12,7 +12,7 @@ const userModel = require('../models/user.m');
 // Start here
 
 passport.serializeUser((user, done) => {
-    done(null, { username: user.username, role: user.role });
+    done(null, { id: user.id, username: user.username, role: user.role });
 });
 passport.deserializeUser(async (user, done) => {
 
