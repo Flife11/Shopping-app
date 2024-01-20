fa99c6139d9b89934139af5ee5041f385ff0d456
Le Sang <73180106+Flife11@users.noreply.github.com>
2024-01-20 17:17:48 +0700

    Delete Main/models/SubCategory.m.js

--- Main/models/SubCategory.m.js
+++
@@ -1,9 +0,0 @@
-const tbName = 'SUBCATEGORY';
-const colName = ['id', 'catid', 'name'];
-module.exports = class SUBCATEGORY {
-    constructor(p) {
-        this.id = p.id || "",
-        this.catid = p.catid || "",
-        this.name = p.name || ""
-    }
-}
