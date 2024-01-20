1f590baa7bffab8b68a6f6e4baf0011752d910f0
21120544 <lesangbt99@gmail.com>
2024-01-20 17:13:43 +0700

    fix conflict duplicate file name

--- Main/models/subcategory.m.js
+++ Main/models/subcategory.m.js
@@ -1,22 +1,9 @@
-const db = require('../utilities/db');
-
-module.exports = {
-    getAll: async () => {
-        try {
-            const sql = `SELECT * FROM "SUBCATEGORY"`;
-            const result = await db.db.query(sql);
-            return result;
-        } catch (error) {
-            console.log(error);
-        }
-    },
-    getSubcategory: async (id) => {
-        try {
-            const sql = `SELECT * FROM "SUBCATEGORY" WHERE id = ${id}`;
-            const result = await db.db.oneOrNone(sql);
-            return result;
-        } catch (error) {
-            console.log(error);
-        }
-    },
+const tbName = 'SUBCATEGORY';
+const colName = ['id', 'catid', 'name'];
+module.exports = class SUBCATEGORY {
+    constructor(p) {
+        this.id = p.id || "",
+        this.catid = p.catid || "",
+        this.name = p.name || ""
+    }
 }
