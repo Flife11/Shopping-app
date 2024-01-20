e5cb9f4bc3528ab1b456725e8c6a0ede8d80f978
21120551 <damtutam@gmail.com>
2024-01-20 14:34:11 +0700

    handle user input at product_detail ; cart (80%)

--- Main/routers/client.r.js
+++ Main/routers/client.r.js
@@ -13,7 +13,6 @@ router.get('/:productid', clientController.getProductDetail);
 // Render list product by category and subcategory 
 // if subcategory = -1 => render list product by only category
 // if category = -1 => render product by params search (apply filter later) (nhớ xử lý trg hợp ng dùng cố tình xóa params search >:c )
-// TODO: xử lý parameter cho page
 router.get('/:catid/:subcatid', clientController.getListProduct);
 
 router.get('/', clientController.getHome);
