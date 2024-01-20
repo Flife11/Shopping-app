11d28913a21a65433a7efb3c6f0e87cf2059b263
21120544 <lesangbt99@gmail.com>
2024-01-20 17:10:35 +0700

    delete products 20%

--- Main/models/category.m.js
+++ Main/models/category.m.js
@@ -1,23 +1,8 @@
-const db = require('../utilities/db');
-
-module.exports = {
-    getAll: async () => {
-        try {
-            const sql = `SELECT * FROM "CATEGORY"`;
-            const result = await db.db.query(sql);
-            return result;
-        } catch (error) {
-            console.log(error);
-        }
-    },
-    getCategory: async (id) => {
-        try {
-            const sql = `SELECT * FROM "CATEGORY" WHERE id = ${id}`;
-            const result = await db.db.oneOrNone(sql);
-            return result;
-        } catch (error) {
-            console.log(error);
-        }
-    },
-
+const tbName = 'CATEGORY';
+const colName = ['id', 'name'];
+module.exports = class CATEGORY {
+    constructor(p) {
+        this.id = p.id || '',
+        this.name = p.name || ''
+    }
 }
